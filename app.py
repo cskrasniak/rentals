acd31c889583ec1a6ab4d32214b814d33753384a
Christopher Krasniak <ckrasnia@cshl.edu>
2022-09-09 12:26:39 -0400

    added nltk download omw

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 # from sklearn.pipeline import FeatureUnion
 # from sklearn.pipeline import Pipeline
